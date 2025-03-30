41827ed9e2cf4ed358b1968d06593363e2556c2a
popeye0618 <popeye0618@gmail.com>
2025-03-31 00:49:00 +0900

    Initial commit

---
+++ src/main/java/seoul/seoulfest/SeoulFestApplication.java
@@ -0,0 +1,13 @@
+package seoul.seoulfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seoul/seoulfest/SeoulFestApplicationTests.java
@@ -0,0 +1,13 @@
+package seoul.seoulfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
