7cca9895551c3c53477b86d4527cfce93371fde6
popeye0618 <popeye0618@gmail.com>
2025-04-17 13:51:12 +0900

    build: TimeZone 서울로 설정

--- src/main/java/seoul/seoulfest/SeoulFestApplication.java
+++ src/main/java/seoul/seoulfest/SeoulFestApplication.java
@@ -1,5 +1,7 @@
 package seoul.seoulfest;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SeoulFestApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(SeoulFestApplication.class, args);
 	}
 
